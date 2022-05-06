2c0538274e84741a5dcce70a50ff10a06b6ab25c
Anik-the-dev <niksctg00@gmail.com>
2022-05-06 21:10:47 +0600

    update btn done

--- src/components/Inventory/DisplayItemCard.js
+++ src/components/Inventory/DisplayItemCard.js
@@ -1,13 +1,108 @@
+import { Button } from 'react-bootstrap';
 import React, { useState } from 'react';
 import { Card, Col, Container, Form, ListGroup, Row } from 'react-bootstrap';
 
 const DisplayItemCard = ({ singleItem }) => {
-    const { image, name, description, quantity, price, sold, supplierName } = singleItem
-    const [addquantity, setAddQuantity] = useState(0)
+    const { _id, image, name, description, quantity, price, sold, supplierName } = singleItem
 
-    const handleQuantityForm = () => {
+    const [addquantity, setAddQuantity] = useState(0)
+    const [delivered, setDelivered] = useState(sold)
+    const [change, setChange] = useState({})
+    console.log("delivered",delivered)
+
+
+    // const handleDeliveredProducts = () => {
+    //     const newDelivered = parseInt(sold) + 1
+    //     setDelivered(newDelivered)
+    //     console.log(delivered)
+
+    //     const quantityAfterDeliver = parseInt(quantity) - 1
+    //     setAddQuantity(quantityAfterDeliver)
+
+    //     const data = { addquantity, delivered }
+    //     console.log(data)
+
+    //     fetch(`http://localhost:5000/users/${_id}`, {
+    //         method: 'PUT',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //         body: JSON.stringify(data),
+    //     })
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log('Update Success:', data);
+    //             alert("Product Delivered Successfully!")
+               
+    //             if (data.acknowledged) {
+    //                 fetch(`http://localhost:5000/users/${_id}`)
+    //                     .then(res => res.json())
+    //                     .then(data => setChange(data))
+
+
+    //             }
+
+
+    //         })
+    //         .catch((error) => {
+    //             console.error('Error:', error);
+    //         });
+
+    // }
+
+
+
+
+
+
+
+    const handleQuantityForm = (e) => {
+        e.preventDefault()
+        const n = parseInt(addquantity) + parseInt(quantity)
+
+        const data = { n, delivered }
+        console.log("data",data)
+        
+        
+
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Update Success:', data);
+                alert("Product Delivered Successfully!")
+                e.target.reset()
+                if (data.acknowledged) {
+                    fetch(`http://localhost:5000/users/${_id}`)
+                        .then(res => res.json())
+                        .then(data => {setChange(data)
+                        console.log("Change",change)})
+
+
+
+                }
+
+
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
 
     }
+
+   
+
+
+
+
+
+
+
     return (
         <Container>
             <h1 className='mt-5 fw-6 fs-3 text-md-start text-center' style={{ color: "#2F2869" }}>Product: {name}</h1>
@@ -34,7 +129,11 @@ const DisplayItemCard = ({ singleItem }) => {
                             Quantity: {quantity}
                         </ListGroup.Item>
                         <ListGroup.Item as="li" className='text-start'>Supplier: {supplierName}</ListGroup.Item>
-                        <ListGroup.Item as="li" className='text-start'>Sold: {sold}</ListGroup.Item>
+                        <ListGroup.Item as="li" className='text-start d-flex align-items-center justify-content-between'>
+                            <p>Sold: {sold}</p>
+                            {/* onClick={() => handleDeliveredProducts()} */}
+                            <Button  className='bg-danger border-0'>Delivered</Button>
+                        </ListGroup.Item>
 
 
                     </ListGroup>
@@ -47,7 +146,7 @@ const DisplayItemCard = ({ singleItem }) => {
 
                             </Form.Control>
 
-                            <input type="submit" value="Add Quantity" className="btn btn-primary w-50 p-2 m-3" style={{ backgroundColor: "#21C9B6", border: "none" }}></input>
+                            <input type="submit" value="Add" className="btn btn-primary w-50 p-2 m-3" style={{ backgroundColor: "#21C9B6", border: "none" }}></input>
 
 
                             {/* <div className='mt-3 d-flex flex-wrap-reverse align-items-center justify-content-between'>
